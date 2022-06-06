1b7d89810e941d8bb8d1b57eb062c1560cf59af9
RiddhishLogistic <riddhish@logisticinfotech.co.in>
2022-06-06 17:45:33 +0530

    call voting function using ethers js

--- pages/components/Coin.js
+++ pages/components/Coin.js
@@ -4,6 +4,9 @@ import { Button } from "web3uikit";
 import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
 import { deployedContractAddress } from "../../config";
 
+import { ethers } from "ethers";
+import MarketSentiment from "../../artifacts/contracts/MarketSentiment.sol/MarketSentiment.json";
+
 function Coin({ perc, setBtc, token, setVisible, setModelToken }) {
   const [color, setColor] = useState();
 
@@ -18,46 +21,64 @@ function Coin({ perc, setBtc, token, setVisible, setModelToken }) {
     }
   }, [perc]);
 
-  async function vote(upDown) {
-    let options = {
-      contractAddress: deployedContractAddress,
-      functionName: "vote",
-      abi: [
-        {
-          inputs: [
-            {
-              internalType: "string",
-              name: "_ticker",
-              type: "string",
-            },
-            {
-              internalType: "bool",
-              name: "_vote",
-              type: "bool",
-            },
-          ],
-          name: "vote",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-      ],
-      params: {
-        _ticker: token,
-        _vote: upDown,
-      },
-    };
+  // async function vote(upDown) {
+  //   let options = {
+  //     contractAddress: deployedContractAddress,
+  //     functionName: "vote",
+  //     abi: [
+  //       {
+  //         inputs: [
+  //           {
+  //             internalType: "string",
+  //             name: "_ticker",
+  //             type: "string",
+  //           },
+  //           {
+  //             internalType: "bool",
+  //             name: "_vote",
+  //             type: "bool",
+  //           },
+  //         ],
+  //         name: "vote",
+  //         outputs: [],
+  //         stateMutability: "nonpayable",
+  //         type: "function",
+  //       },
+  //     ],
+  //     params: {
+  //       _ticker: token,
+  //       _vote: upDown,
+  //     },
+  //   };
 
-    await contractProcess.fetch({
-      params: options,
-      onSuccess: () => {
-        console.log("vote successfully");
-      },
-      onError: (error) => {
-        console.log("error response : ", error);
-        alert(error);
-      },
-    });
+  //   await contractProcess.fetch({
+  //     params: options,
+  //     onSuccess: () => {
+  //       console.log("vote successfully");
+  //     },
+  //     onError: (error) => {
+  //       console.log("error response : ", error);
+  //       alert(error);
+  //     },
+  //   });
+  // }
+
+  async function vote(upDown) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    console.log("signer : ", signer);
+    let contract = new ethers.Contract(
+      deployedContractAddress,
+      MarketSentiment.abi,
+      signer
+    );
+    try {
+      let voting = await contract.vote(token, upDown);
+      console.log(voting);
+    } catch (error) {
+      console.log("error : ", JSON.stringify(error));
+      alert(JSON.stringify(error.reason));
+    }
   }
 
   return (
